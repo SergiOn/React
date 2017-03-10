5cfc978aaced2579065ea5ff7bfb1db753e86736
SergiOn <onishhenko@gmail.com>
2017-03-10 17:03:03 +0200

    #1 hello react #add image, subtitle, container. change element to title. wrap tags in container

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-var element = React.createElement('h1', null, 'Hello React!');
+let image = React.createElement('img', { src: 'https://facebook.github.io/react/img/logo.svg' });
+let title = React.createElement('h1', null, 'React');
+let subtitle = React.createElement('p', null, 'A javascript library for building user interfaces');
+let container = React.createElement('div', { className: 'container' }, image, title, subtitle);
 
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(container, document.getElementById('root'));
