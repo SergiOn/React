88f727ecadd6e0b4071858bab8589215c8f6a186
SergiOn <Onishhenko@gmail.com>
2017-03-21 01:24:01 +0200

    #6 properties (props) # plus angular component

--- app.jsx
+++ app.jsx
@@ -8,8 +8,15 @@ function Hero(props) {
     );
 }
 
-ReactDOM.render(<Hero title="React"
-                      subtitle="A javascript library for building user interface"
-                      imageUrl="https://facebook.github.io/react/img/logo.svg"
-                />,
-                document.getElementById('root'));
+ReactDOM.render(
+    <div className="container-wrapper">
+        <Hero title="React"
+              subtitle="A javascript library for building user interface"
+              imageUrl="https://facebook.github.io/react/img/logo.svg"
+        />
+        <Hero title="Angular 2"
+              subtitle="One framework"
+              imageUrl="https://angular.io/resources/images/logos/angular2/angular.svg"
+        />
+    </div>,
+    document.getElementById('root'));
