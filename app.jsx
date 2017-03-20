b43f5ef3dd9f5823653d33e278ce030ee56a53c4
SergiOn <Onishhenko@gmail.com>
2017-03-21 00:52:19 +0200

    #5 components

--- app.jsx
+++ app.jsx
@@ -1,11 +1,11 @@
-let jsx =
-<div className="container">
-    <img src="https://facebook.github.io/react/img/logo.svg" />
-    <h1>React</h1>
-    <p>A javascript library for building user interface</p>
-</div>;
+function Hero() {
+    return (
+        <div className="container">
+            <img src="https://facebook.github.io/react/img/logo.svg" />
+            <h1>React</h1>
+            <p>A javascript library for building user interface</p>
+        </div>
+    );
+}
 
-let dom = ReactDOM.render(jsx, document.getElementById('root'));
-
-console.log(jsx);
-console.log(dom);
+ReactDOM.render(Hero(), document.getElementById('root'));
