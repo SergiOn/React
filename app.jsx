4c6d33ccf3f9e9e8b12341bc7dbb9eb2c8f14275
SergiOn <Onishhenko@gmail.com>
2017-03-21 01:56:20 +0200

    #7 state # React.createClass

--- app.jsx
+++ app.jsx
@@ -1,23 +1,46 @@
-function Hero(props) {
-    let count = 0;
+// function Hero(props) {
+//     let count = 0;
+//
+//     function handleClick() {
+//         count += 1;
+//         console.log(count);
+//         console.log('click');
+//     }
+//
+//     return (
+//         <div className="container">
+//             <div className="count">{count}</div>
+//             <img src={props.imageUrl} onClick={handleClick} />
+//             <h1>{props.title}</h1>
+//             <p>{props.subtitle}</p>
+//         </div>
+//     );
+// }
 
-    function handleClick() {
-        count += 1;
-        console.log(count);
+let Hero = React.createClass({
+    getInitialState: function () {
+        return {
+            count: 0
+        };
+    },
+
+    handleClick: function () {
+        this.state.count += 1;
+        console.log(this.state.count);
         console.log('click');
+    },
+
+    render: function () {
+        return (
+            <div className="container">
+                <div className="count">{this.state.count}</div>
+                <img src={this.props.imageUrl} onClick={this.handleClick} />
+                <h1>{this.props.title}</h1>
+                <p>{this.props.subtitle}</p>
+            </div>
+        );
     }
-
-    return (
-        <div className="container">
-            <div className="count">{count}</div>
-            <img src={props.imageUrl} onClick={handleClick} />
-            <h1>{props.title}</h1>
-            <p>{props.subtitle}</p>
-        </div>
-    );
-}
-
-// React.createClass({});
+});
 
 ReactDOM.render(
     <div className="container-wrapper">
